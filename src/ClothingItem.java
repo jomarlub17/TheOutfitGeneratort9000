9cbacd73fd7e8a29a8af546a3be44e5c6b4cf0a4
jomarlub17 <161365974+jomarlub17@users.noreply.github.com>
2025-06-11 12:45:12 -0400

    Started the outfit generator.

---
+++ src/ClothingItem.java
@@ -0,0 +1,13 @@
+public class ClothingItem {
+    private String type;
+    private String color;
+    private String material;
+    private boolean isClean;
+
+    public ClothingItem(String type, String color, String material) {
+        this.type = type;
+        this.color = color;
+        this.material = material;
+        this.isClean = true;
+    }
+}
