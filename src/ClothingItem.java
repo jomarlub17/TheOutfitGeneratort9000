62b8c078fab56c974c55edfa0b3357ef51689c5d
jomarlub17 <161365974+jomarlub17@users.noreply.github.com>
2025-07-15 12:24:03 -0400

    Started the outfit generator.

--- src/ClothingItem.java
+++ src/ClothingItem.java
@@ -2,12 +2,29 @@ public class ClothingItem {
     private String type;
     private String color;
     private String material;
+    private String season;
     private boolean isClean;
 
-    public ClothingItem(String type, String color, String material) {
+    public ClothingItem(String type, String color, String material, String season) {
         this.type = type;
         this.color = color;
         this.material = material;
+        this.season = season;
         this.isClean = true;
     }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setColor(String coloor) {
+        this.color = color;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+    public void setSeason(String season) {
+        this.season = season;
+    }
 }
+ 
