5aa0e71ef8d09ffc5effe6eb798cc7bfba0125f4
jomarlub17 <161365974+jomarlub17@users.noreply.github.com>
2025-09-17 23:29:16 -0400

    Started working on the collection and files to store and save clothes

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App{
     public static void main(String[] args) {
         System.out.println("Welcome to your personal wardrobe app");
         ClothingItem item1 = new ClothingItem();
+        Wardrobe item2 = new Wardrobe();
     }
 }
--- src/Wardrobe.java
+++ src/Wardrobe.java
@@ -1,10 +1,16 @@
 import java.io.File;
-import java.io.FileWriter;
-import java.io.FileReader;
 import java.util.ArrayList;
 public class Wardrobe {
+    //Goal: get the clothing items from the user into the file. and read them from file as needed.
     //Create an arraylist to store the clothing items
     ArrayList<String> listOfClothes = new ArrayList<>();
     //create a file to handle data
-    File CollectStorage = new File("wardrobe.txt");
+    public Wardrobe(){
+        try{
+            File storage = new File("Wardrobe.txt");
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
 }
